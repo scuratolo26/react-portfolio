d75ec2a86623bc57d2524d521e7bfc2453b34f4e
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-10-24 14:41:43 -0400

    portfolio component & new favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Portfolio></Portfolio>
         <About></About>
       </main>
     </div>
