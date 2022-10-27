96cbcf7144a663860c1394cd5b19f320af7a8821
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-10-26 21:23:59 -0400

    add test resume link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import About from './components/About';
-import ContactForm from './components/Contact';
 import Nav from './components/Nav';
-import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
-        <Portfolio></Portfolio>
         <About></About>
         <ContactForm></ContactForm>
+        <Resume></Resume>
       </main>
     </div>
   );
---
+++ src/components/Resume/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section>
+            <h1>Resume</h1>
+            <a href="https://drive.google.com/file/d/1z-P5zHg6wDM4VbfBcfDGlqyzr301ma8b/view?usp=sharing"
+                target="_blank" rel="noreferrer">Click here to open resume</a>
+        </section>
+    )
+}
+
+export default Resume;
