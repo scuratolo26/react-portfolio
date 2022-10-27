c228654018265479bd52d9ea63a2f18f705ef01e
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-10-27 16:15:44 -0400

    portfolio info and photos

--- src/assets/portfolio/index.js
+++ src/assets/portfolio/index.js
@@ -1,9 +1,17 @@
-import proPic from "./propic.png";
-import gitHub from "./GitHub.png";
+import foodLovers from "./food-lovers.png";
+import noteTaker from "./note-taker.png";
+import techBlog from "./tech-blog.png";
+import pizzaHunt from "./pizza-hunt.png";
+import textEditor from "./text-editor.png";
+import zooKeeper from "./zoo-keeper.png";
 
 const images = {
-    proPic,
-    gitHub,
+    foodLovers,
+    noteTaker,
+    techBlog,
+    pizzaHunt,
+    textEditor,
+    zooKeeper,
 };
 
 export default images;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,32 +4,60 @@ import images from "../../assets/portfolio";
 function Portfolio() {
     const projects = [
         {
-            name: "Project 1",
-            description: "project 1 @ git",
-            imgSrc: images.proPic,
-            gitHub: "github.com",
-            deployed: "deployed.com"
+            name: "Food Lovers",
+            imgSrc: images.foodLovers,
+            gitHub: "https://github.com/scuratolo26/Food-Lovers",
+            deployed: "https://food-lovers-project.herokuapp.com/"
         },
         {
-            name: "Project 2",
-            description: "project 2 @ git",
-            imgSrc: images.gitHub,
-            gitHub: "github.com",
-            deployed: "deployed.com"
-        }
+            name: "Note Taker",
+            imgSrc: images.noteTaker,
+            gitHub: "https://github.com/scuratolo26/note-taker",
+            deployed: "https://note-taker-scuratolo26.herokuapp.com/"
+        },
+        {
+            name: "Tech Blog",
+            imgSrc: images.techBlog,
+            gitHub: "https://github.com/scuratolo26/tech-blog",
+            deployed: "https://vast-ridge-87358.herokuapp.com/"
+        },
+        {
+            name: "Pizza Hunt",
+            imgSrc: images.pizzaHunt,
+            gitHub: "https://github.com/scuratolo26/pizza-hunt",
+            deployed: "https://sc-pizza-hunt.herokuapp.com/"
+        },
+        {
+            name: "Text Editor PWA",
+            imgSrc: images.textEditor,
+            gitHub: "https://github.com/scuratolo26/TextEditor-PWA",
+            deployed: "https://fast-basin-68082.herokuapp.com/"
+        },
+        {
+            name: "Zoo Keeper",
+            imgSrc: images.zooKeeper,
+            gitHub: "https://github.com/scuratolo26/zookeeper",
+            deployed: "https://limitless-gorge-36474.herokuapp.com/"
+        },
     ];
 
     return (
-        <ul>
-            {projects.map(project => {
-                return (
-                    <li key={project.name}>
-                        {project.name} - {project.description},  {project.gitHub}, {project.deployed},
-                        <img src={project.imgSrc} alt={project.name} />
-                    </li>
-                );
-            })}
-        </ul>
+        <div>
+            <h2>Projects</h2>
+            <div className="flex-row">
+                {projects.map(project => {
+                    return (
+                        <div key={project.name} className="card">
+                            <img src={project.imgSrc} alt={project.name} className="project-img" />
+                            <div className="container">
+                                <h2 className="card-header">{project.name}</h2>
+                                <button className="card-button"><a href={project.gitHub} target="_blank" rel="noreferrer">GitHub</a></button> <button className="card-button"><a href={project.deployed} target="_blank" rel="noreferrer">Deployed Application</a></button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     )
 }
 
